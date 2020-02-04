46c9220b6014e6a986b8ec59e4ce26becc62d4ab
zakariabijoy <zakaria.bijoy@live.com>
2020-02-05 01:33:26 +0600

    Add project files.

---
+++ HotelManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManagementSystem.Startup))]
+namespace HotelManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
