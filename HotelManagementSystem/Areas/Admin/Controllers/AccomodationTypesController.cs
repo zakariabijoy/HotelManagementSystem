a14b8aa4acd96b268577ea974bc55918d6150991
zakariabijoy <zakaria.bijoy@live.com>
2020-03-29 02:18:01 +0600

    accomodation edit and delete functionality is added

--- HotelManagementSystem/Areas/Admin/Controllers/AccomodationTypesController.cs
+++ HotelManagementSystem/Areas/Admin/Controllers/AccomodationTypesController.cs
@@ -1,6 +1,7 @@
 ﻿using HotelManagementSystem.Areas.Admin.ViewModel;
 using HotelManagementSystem.Models;
 using System.Linq;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace HotelManagementSystem.Areas.Admin.Controllers
@@ -21,24 +22,73 @@ namespace HotelManagementSystem.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public ActionResult Action()
+        public ActionResult Action(int? id)
         {
-            var model = new AccomodationTypeActionViewModel();
-            return PartialView("_Action",model);
+            if (id == null)                                        //create form
+            {
+                var model = new AccomodationTypeActionViewModel();
+                return PartialView("_Action", model);
+            }
+            else                                                   // edit form
+            {
+                var accomodationType = _context.AccomodationTypes.Find(id);
+                var model = new AccomodationTypeActionViewModel()
+                {
+                    Id = accomodationType.Id,
+                    Name = accomodationType.Name,
+                    Description = accomodationType.Description
+                };
+                return PartialView("_Action", model);
+            }
+            
         }
 
         [HttpPost]
         public ActionResult Action(AccomodationType model)
         {
-            var accomodationType = new AccomodationType();
+            if (model.Id > 0)                           // edit a accommodation type
+            {
+                var accomodationType = _context.AccomodationTypes.Find(model.Id);
+                accomodationType.Name = model.Name;
+                accomodationType.Description = model.Description;
+            }
+            else                                          // create a new accommodation type
+            {
+                var accomodationType = new AccomodationType();
 
-            accomodationType.Name = model.Name;
-            accomodationType.Description = model.Description;
+                accomodationType.Name = model.Name;
+                accomodationType.Description = model.Description;
 
-            _context.AccomodationTypes.Add(accomodationType);
+                _context.AccomodationTypes.Add(accomodationType);
+            }
+
             _context.SaveChanges();
 
             return Json(new {success = true}, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var accomodationType = _context.AccomodationTypes.Find(id);
+            var model = new AccomodationTypeActionViewModel()
+            {
+                Id = accomodationType.Id
+                
+            };
+            return PartialView("_Delete", model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(AccomodationType model)
+        {
+
+            var accomodationType = _context.AccomodationTypes.Find(model.Id);
+
+            _context.AccomodationTypes.Remove(accomodationType);
+            _context.SaveChanges();
+
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
         }
     }
 }
